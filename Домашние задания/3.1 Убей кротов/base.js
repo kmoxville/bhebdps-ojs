cdd4f64846d031f9aaada26e4392e018915e3d89
Alexander Samilovskikh <alex.kmoxville@gmail.com>
2024-12-17 14:08:48 +0300

    Update base.js
    Homework 10

--- Домашние задания/3.1 Убей кротов/base.js
+++ Домашние задания/3.1 Убей кротов/base.js
@@ -1,22 +1,25 @@
-(() => {
-  let playing = true,
-    activeHole = 1;
-
-  const stop = () => playing = true,
-    getHole = index => document.getElementById(`hole${index}`),
-    deactivateHole = index =>
-      getHole( index ).className = 'hole',
-    activateHole = index =>
-      getHole( index ).className = 'hole hole_has-mole',
-    next = () => setTimeout(() => {
-      if ( !playing ) {
-        return;
+document.querySelectorAll('.hole').forEach(hole => {
+  hole.addEventListener('click', function() {
+      let deadElement = document.getElementById('dead');
+      let lostElement = document.getElementById('lost');
+      if (this.classList.contains('hole_has-mole')) {
+          if (deadElement) {
+              deadElement.textContent = (parseInt(deadElement.textContent) + 1).toString();
+          }
+          if (parseInt(deadElement.textContent) === 10) {
+              alert('You win');
+              deadElement.textContent = '0';
+              lostElement.textContent = '0';
+          }
+      } else {
+          if (lostElement) {
+              lostElement.textContent = (parseInt(lostElement.textContent) + 1).toString();
+          }
+          if (parseInt(lostElement.textContent) === 5) {
+              alert('You lost');
+              deadElement.textContent = '0';
+              lostElement.textContent = '0';
+          }
       }
-      deactivateHole( activeHole );
-      activeHole = Math.floor( 1 + Math.random() * 9 );
-      activateHole( activeHole );
-      next();
-    }, 800 );
-
-  next();
-})();
+  });
+});
